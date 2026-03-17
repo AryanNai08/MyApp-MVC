533be2b679ccc8f9137bbd74be4e3df358ae404f
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-17 11:25:07 +0530

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp_MVC.Models;
+
+namespace MyApp_MVC.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "Aryan" };
+            return View(item);
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace MyApp_MVC.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
