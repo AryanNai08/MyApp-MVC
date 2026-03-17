fbc7d0fd06d7af2fa1c5d561e3646f8fe3ed8980
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-17 11:52:43 +0530

    item name in Overview method

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -7,7 +7,7 @@ namespace MyApp_MVC.Controllers
     {
         public IActionResult Overview()
         {
-            var item = new Item() { Name = "Aryan limbachiya" };
+            var item = new Item() { Name = "Aryan b limbachiya" };
             return View(item);
         }
 
