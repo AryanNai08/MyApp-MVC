270b168d22029c99fef62b144077f5407c6bd6e5
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-17 17:47:54 +0530

    (migration)-migration files

--- Migrations/20260317065751_PriceColumnAdded.Designer.cs
+++ Migrations/20260317065751_PriceColumnAdded.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyApp_MVC.Data;
 
@@ -10,9 +11,11 @@ using MyApp_MVC.Data;
 namespace MyApp_MVC.Migrations
 {
     [DbContext(typeof(MyAppDbContext))]
-    partial class MyAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260317065751_PriceColumnAdded")]
+    partial class PriceColumnAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Item.cs
+++ Models/Item.cs
@@ -2,7 +2,9 @@
 {
     public class Item
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
     }
 }
