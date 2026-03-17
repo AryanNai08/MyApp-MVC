4fb01d25655a5f643d734c80369bed6fb249e267
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-03-17 17:49:33 +0530

    (cshtml & css) :-created diffrent views and apply css

---
+++ Views/Items/GetAllItems.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp_MVC.Views.Items
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
